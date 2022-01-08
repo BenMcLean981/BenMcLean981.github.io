cd0a57fd7eec2e7ed178c92e9333f7be567d3bcd
Ben McLean <bmcle071@uottawa.ca>
2022-01-08 14:43:13 -0500

    Upgrading to node 16. Decided that switching early would be a good call.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import App from "./App";
+import { App } from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
