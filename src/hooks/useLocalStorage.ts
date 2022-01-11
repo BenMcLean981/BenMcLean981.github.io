b1b01d852246df5871b360eca434e3cce62bff95
Ben McLean <bmcle071@uottawa.ca>
2022-01-11 12:01:35 -0500

    fixing media query, wondering how I can test it automatically

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -6,15 +6,17 @@ export function useLocalStorage(
 ): [string, React.Dispatch<React.SetStateAction<string>>] {
   function getLocalStorage(key: string, defaultValue: string): string {
     const item = window.localStorage.getItem(key);
+    console.log("item", item);
 
     if (item) return item;
     else {
+      console.log("defaultValue", defaultValue);
       window.localStorage.setItem(key, defaultValue);
       return defaultValue;
     }
   }
 
-  const [value, setValue] = useState(() => getLocalStorage(key, defaultValue));
+  const [value, setValue] = useState(getLocalStorage(key, defaultValue));
 
   useEffect(() => {
     window.localStorage.setItem(key, value);
--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 
 export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(
+    window.matchMedia(query).matches !== null
+  );
 
   useEffect(() => {
     const media = window.matchMedia(query);
