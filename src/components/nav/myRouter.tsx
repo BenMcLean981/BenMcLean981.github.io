9c129176ecce7652ed6b28c9b34e67c98d6a73ca
Ben McLean <bmcle071@uottawa.ca>
2022-01-11 16:52:15 -0500

    trying to fix github pages 404

--- src/components/nav/myRouter.tsx
+++ src/components/nav/myRouter.tsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Home } from "../../pages/home";
-import { Minesweeper } from "../../pages/minesweeper"
+import { Minesweeper } from "../../pages/minesweeper";
 
 export function MyRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/resume">Resume</Route>
-        <Route path="/minesweeper" element={<Minesweeper />}>Minesweeper</Route>
+        <Route path="/minesweeper" element={<Minesweeper />}>
+          Minesweeper
+        </Route>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
