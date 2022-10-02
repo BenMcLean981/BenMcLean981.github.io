6f6057af58b251cd969fd124dff8265ee704b2b7
BenMcLean981 <BenMcLean981@gmail.com>
2022-10-02 19:17:18 -0400

    Fixing bug.

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,19 @@ import "./index.css";
 
 import { App } from "./App";
 import React from "react";
-import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-
+import ReactDOM from "react-dom";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import reportWebVitals from "./reportWebVitals";
 
 library.add(fab);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
