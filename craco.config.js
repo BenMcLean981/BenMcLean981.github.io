b08fd5860b172d647f51ac24178b54cf1895d5e2
BenMcLean981 <BenMcLean981@gmail.com>
2023-02-23 21:43:53 -0500

    Prepped for wasm.

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,27 @@
 // craco.config.js
 module.exports = {
   style: {
-    postcss: {
+    postcssOptions: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: (config) => {
+      const wasmExtensionRegExp = /\.wasm$/;
+      config.resolve.extensions.push(".wasm");
+      config.experiments = {
+        syncWebAssembly: true,
+      };
+
+      config.module.rules.forEach((rule) => {
+        (rule.oneOf || []).forEach((oneOf) => {
+          if (oneOf.type === "asset/resource") {
+            oneOf.exclude.push(wasmExtensionRegExp);
+          }
+        });
+      });
+
+      return config;
     },
   },
 };
